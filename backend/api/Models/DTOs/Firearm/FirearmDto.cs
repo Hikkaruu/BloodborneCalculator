d30869e10040cfd244a84c61ed4eb0715257fe5c
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-04-18 19:25:00 +0200

    Add Firearm DTOs

---
+++ backend/api/Models/DTOs/Firearm/FirearmDto.cs
@@ -0,0 +1,64 @@
+﻿using api.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models.DTOs.Firearm
+{
+    public class FirearmDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int PhysicalAttack { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int BloodAttack { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int ArcaneAttack { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int FireAttack { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int BoltAttack { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int BulletUse { get; set; }
+
+        [Required]
+        public ImprintType Imprints { get; set; }
+
+        [Required]
+        [Range(0, 99)]
+        public int StrengthRequirement { get; set; }
+
+        [Required]
+        [Range(0, 99)]
+        public int SkillRequirement { get; set; }
+
+        [Required]
+        [Range(0, 99)]
+        public int BloodtingeRequirement { get; set; }
+
+        [Required]
+        [Range(0, 99)]
+        public int ArcaneRequirement { get; set; }
+
+        [Required]
+        [Url]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public int ScalingId { get; set; }
+        public string? ScalingName { get; set; }
+    }
+}
---
+++ backend/api/Models/DTOs/Firearm/UpdateFirearmDto.cs
@@ -0,0 +1,50 @@
+﻿using api.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models.DTOs.Firearm
+{
+    public class UpdateFirearmDto
+    {
+        [MaxLength(50, ErrorMessage = "Name can't be longer than 50 characters")]
+        public string? Name { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Physical Attack must be between 0 and 1000")]
+        public int? PhysicalAttack { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Blood Attack must be between 0 and 1000")]
+        public int? BloodAttack { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Arcane Attack must be between 0 and 1000")]
+        public int? ArcaneAttack { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Fire Attack must be between 0 and 1000")]
+        public int? FireAttack { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Bolt Attack must be between 0 and 1000")]
+        public int? BoltAttack { get; set; }
+
+        [Range(0, 20, ErrorMessage = "Bullet Use must be between 0 and 20")]
+        public int? BulletUse { get; set; }
+
+        public ImprintType? Imprints { get; set; }
+
+        [Range(0, 99, ErrorMessage = "Strength Requirement must be between 0 and 99")]
+        public int? StrengthRequirement { get; set; }
+
+        [Range(0, 99, ErrorMessage = "Skill Requirement must be between 0 and 99")]
+        public int? SkillRequirement { get; set; }
+
+        [Range(0, 99, ErrorMessage = "Bloodtinge Requirement must be between 0 and 99")]
+        public int? BloodtingeRequirement { get; set; }
+
+        [Range(0, 99, ErrorMessage = "Arcane Requirement must be between 0 and 99")]
+        public int? ArcaneRequirement { get; set; }
+
+        [Url(ErrorMessage = "Provide valid URL")]
+        [MaxLength(500, ErrorMessage = "URL can't be longer than 500 characters")]
+        public string? ImageUrl { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Scaling Id can't be negative")]
+        public int? ScalingId { get; set; }
+    }
+}
