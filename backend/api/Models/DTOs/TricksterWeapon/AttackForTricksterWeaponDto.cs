a8cfb2d26957d6eb9802853e11282faafb52ec3a
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-04-18 19:57:31 +0200

    Add TricksterWeapons DTOs

---
+++ backend/api/Models/DTOs/TricksterWeapon/AttackForTricksterWeaponDto.cs
@@ -0,0 +1,22 @@
+﻿using api.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models.DTOs.TricksterWeapon
+{
+    public class AttackForTricksterWeaponDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Damage { get; set; }
+
+        public decimal ExtraDamage { get; set; }
+
+        public int ExtraDamageCount { get; set; }
+
+        public AttackType AttackType1 { get; set; }
+
+        public AttackType? AttackType2 { get; set; }
+
+        public AttackMode AttackMode { get; set; }
+    }
+}
