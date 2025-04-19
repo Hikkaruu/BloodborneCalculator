7c46f084f6b5714a8a44473858ce4d0e8012537a
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-04-19 22:22:01 +0200

    Add Attack, Boss, Scaling interfaces

---
+++ backend/api/Interfaces/IBossService.cs
@@ -0,0 +1,13 @@
+﻿using api.Models.DTOs.Boss;
+
+namespace api.Interfaces
+{
+    public interface IBossService
+    {
+        Task<BossDto> GetBossByIdAsync(int id);
+        Task<IEnumerable<BossDto>> GetAllBossesAsync();
+        Task<BossDto> CreateBossAsync(CreateBossDto createDto);
+        Task<BossDto> UpdateBossAsync(int id, UpdateBossDto updateDto);
+        Task<bool> DeleteBossAsync(int id);
+    }
+}
