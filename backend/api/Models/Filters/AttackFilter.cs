ccb311e3436d09a2f5085836a29decb6488b80aa
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-04-19 22:22:58 +0200

    Add Attack filter

---
+++ backend/api/Models/Filters/AttackFilter.cs
@@ -0,0 +1,26 @@
+﻿using api.Models.Enums;
+
+namespace api.Models.Filters
+{
+    public record AttackFilter(
+        string? WeaponName = null,
+        decimal? Damage = null,
+        decimal? ExtraDamage = null,
+        int? ExtraDamageCount = null,
+        AttackType? AttackType1 = null,
+        AttackType? AttackType2 = null,
+        AttackMode? AttackMode = null,
+        int? WeaponId = null
+    )
+    {
+        public bool HasFilters =>
+            !string.IsNullOrWhiteSpace(WeaponName) ||
+            Damage.HasValue ||
+            ExtraDamage.HasValue ||
+            ExtraDamageCount.HasValue ||
+            AttackType1.HasValue ||
+            AttackType2.HasValue ||
+            AttackMode.HasValue ||
+            WeaponId.HasValue;
+    }
+}
