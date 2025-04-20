27a08f97ad2cf374ba07cb9acd74346f884e7d67
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-04-20 14:10:32 +0200

    Add Firearm service and filter

---
+++ backend/api/Interfaces/IFirearmService.cs
@@ -0,0 +1,15 @@
+﻿using api.Models.DTOs.Firearm;
+using api.Models.Filters;
+
+namespace api.Interfaces
+{
+    public interface IFirearmService
+    {
+        Task<FirearmDto> GetFirearmByIdAsync(int id);
+        Task<IEnumerable<FirearmDto>> GetAllFirearmsAsync();
+        Task<FirearmDto> CreateFirearmAsync(CreateFirearmDto createDto);
+        Task<FirearmDto> UpdateFirearmAsync(int id, UpdateFirearmDto updateDto);
+        Task<bool> DeleteFirearmAsync(int id);
+        Task<IEnumerable<FirearmDto>> GetAttacksByFilterAsync(FirearmFilter filter);
+    }
+}
---
+++ backend/api/Models/Filters/FirearmFilter.cs
@@ -0,0 +1,35 @@
+﻿using api.Models.Enums;
+using System.Xml.Linq;
+
+namespace api.Models.Filters
+{
+    public record FirearmFilter(
+        string? FirearmName = null,
+        int? PhysicalAttack = null,
+        int? BloodAttack = null,
+        int? ArcaneAttack = null,
+        int? FireAttack = null,
+        int? BoltAttack = null,
+        int? BulletUse = null,
+        ImprintType? Imprints = null,
+        int? StrengthRequirement = null,
+        int? SkillRequirement = null,
+        int? BloodtingeRequirement = null,
+        int? ArcaneRequirement = null
+    )
+    {
+        public bool HasFilters =>
+            !string.IsNullOrWhiteSpace(FirearmName) ||
+            PhysicalAttack.HasValue ||
+            BloodAttack.HasValue ||
+            ArcaneAttack.HasValue ||
+            FireAttack.HasValue ||
+            BoltAttack.HasValue ||
+            BulletUse.HasValue ||
+            Imprints.HasValue ||
+            StrengthRequirement.HasValue ||
+            SkillRequirement.HasValue ||
+            BloodtingeRequirement.HasValue ||
+            ArcaneRequirement.HasValue;    
+    }
+}
