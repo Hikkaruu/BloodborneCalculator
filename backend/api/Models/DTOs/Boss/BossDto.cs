4c48a92578a3ab713a7722b3dc35ba96526a88fa
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-04-20 18:58:51 +0200

    Refactor Boss DTOs

--- backend/api/Models/DTOs/Boss/BossDto.cs
+++ backend/api/Models/DTOs/Boss/BossDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api.Models.DTOs.Boss
 {
@@ -21,5 +22,58 @@ namespace api.Models.DTOs.Boss
 
         [Required]
         public bool IsRequired { get; set; }
+
+        [Required]
+        public bool IsBeast { get; set; }
+
+        [Required]
+        public bool IsKin { get; set; }
+
+        [Required]
+        public bool IsWeakToSerrated { get; set; }
+
+        [Required]
+        public bool IsWeakToRighteous { get; set; }
+
+        [Required]
+        [Range(0, 999)]
+        public int PhysicalDefense { get; set; }
+
+        [Required]
+        [Range(0, 999)]
+        public int BluntDefense { get; set; }
+
+        [Required]
+        [Range(0, 999)]
+        public int ThrustDefense { get; set; }
+
+        [Required]
+        [Range(0, 999)]
+        public int BloodtingeDefense { get; set; }
+
+        [Required]
+        [Range(0, 999)]
+        public int ArcaneDefense { get; set; }
+
+        [Required]
+        [Range(0, 999)]
+        public int FireDefense { get; set; }
+
+        [Required]
+        [Range(0, 999)]
+        public int BoltDefense { get; set; }
+
+        [Required]
+        [Range(0, 999)]
+        public int SlowPoisonResistance { get; set; }
+
+        [Required]
+        [Range(0, 999)]
+
+        public int RapidPoisonResistance { get; set; }
+
+        [Required]
+        [Url]
+        public string ImageUrl { get; set; } = string.Empty;
     }
 }
--- backend/api/Models/DTOs/Boss/UpdateBossDto.cs
+++ backend/api/Models/DTOs/Boss/UpdateBossDto.cs
@@ -4,6 +4,9 @@ namespace api.Models.DTOs.Boss
 {
     public class UpdateBossDto
     {
+        [Required]
+        public int Id { get; set; }
+
         [MaxLength(50, ErrorMessage = "Name can't be longer than 50 characters")]
         public string? Name { get; set; }
 
@@ -16,6 +19,59 @@ namespace api.Models.DTOs.Boss
         public bool? IsInterruptible { get; set; }
 
         public bool? IsRequired { get; set; }
+
+        [Required]
+        public bool IsBeast { get; set; }
+
+        [Required]
+        public bool IsKin { get; set; }
+
+        [Required]
+        public bool IsWeakToSerrated { get; set; }
+
+        [Required]
+        public bool IsWeakToRighteous { get; set; }
+
+        [Required]
+        [Range(0, 999, ErrorMessage = "Physical Defense must be between 0 and 999 inclusive")]
+        public int PhysicalDefense { get; set; }
+
+        [Required]
+        [Range(0, 999, ErrorMessage = "Blunt Defense must be between 0 and 999 inclusive")]
+        public int BluntDefense { get; set; }
+
+        [Required]
+        [Range(0, 999, ErrorMessage = "Thrust Defense must be between 0 and 999 inclusive")]
+        public int ThrustDefense { get; set; }
+
+        [Required]
+        [Range(0, 999, ErrorMessage = "Bloodtinge Defense must be between 0 and 999 inclusive")]
+        public int BloodtingeDefense { get; set; }
+
+        [Required]
+        [Range(0, 999, ErrorMessage = "Arcane Defense must be between 0 and 999 inclusive")]
+        public int ArcaneDefense { get; set; }
+
+        [Required]
+        [Range(0, 999, ErrorMessage = "Fire Defense must be between 0 and 999 inclusive")]
+        public int FireDefense { get; set; }
+
+        [Required]
+        [Range(0, 999, ErrorMessage = "Bolt Defense must be between 0 and 999 inclusive")]
+        public int BoltDefense { get; set; }
+
+        [Required]
+        [Range(0, 999, ErrorMessage = "Slow Poison Resistance must be between 0 and 999 inclusive")]
+        public int SlowPoisonResistance { get; set; }
+
+        [Required]
+        [Range(0, 999, ErrorMessage = "Rapid Poison Resistance must be between 0 and 999 inclusive")]
+        public int RapidPoisonResistance { get; set; }
+
+        [Required]
+        [Url(ErrorMessage = "Provide valid URL")]
+        [MaxLength(500, ErrorMessage = "URL can't be longer than 500 characters")]
+        public string ImageUrl { get; set; } = string.Empty;
     }
 }
 
