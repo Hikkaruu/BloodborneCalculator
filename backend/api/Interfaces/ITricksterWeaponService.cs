8535bfc3dd96cb3000a2f8b182c280a8584f8a87
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-04-20 15:13:38 +0200

    Add TricksterWeapon service and filter

---
+++ backend/api/Interfaces/ITricksterWeaponService.cs
@@ -0,0 +1,15 @@
+﻿using api.Models.DTOs.TricksterWeapon;
+using api.Models.Filters;
+
+namespace api.Interfaces
+{
+    public interface ITricksterWeaponService
+    {
+        Task<TricksterWeaponDto> GetTricksterWeaponByIdAsync(int id);
+        Task<IEnumerable<TricksterWeaponDto>> GetAllTricksterWeaponsAsync();
+        Task<TricksterWeaponDto> CreateTricksterWeaponAsync(CreateTricksterWeaponDto createDto);
+        Task<TricksterWeaponDto> UpdateTricksterWeaponAsync(int id, UpdateTricksterWeaponDto updateDto);
+        Task<bool> DeleteTricksterWeaponAsync(int id);
+        Task<IEnumerable<TricksterWeaponDto>> GetTricksterWeaponsByFilterAsync(TricksterWeaponFilter filter);
+    }
+}
---
+++ backend/api/Models/Filters/TricksterWeaponFilter.cs
@@ -0,0 +1,44 @@
+﻿using api.Models.Enums;
+
+namespace api.Models.Filters
+{
+    public record TricksterWeaponFilter(
+        string? TricksterWeaponName = null,
+        int? PhysicalAttack = null,
+        int? BloodAttack = null,
+        int? ArcaneAttack = null,
+        int? FireAttack = null,
+        int? BoltAttack = null,
+        int? BulletUse = null,
+        int? RapidPoison = null,
+        ImprintType? ImprintsNormal = null,
+        ImprintType? ImprintsUncanny = null,
+        ImprintType? ImprintsLost = null,
+        int? Rally = null,
+        int? StrengthRequirement = null,
+        int? SkillRequirement = null,
+        int? BloodtingeRequirement = null,
+        int? ArcaneRequirement = null,
+        int? MaxUpgradeAttack = null
+    )
+    {
+        public bool HasFilters =>
+            !string.IsNullOrWhiteSpace(TricksterWeaponName) ||
+            PhysicalAttack.HasValue ||
+            BloodAttack.HasValue ||
+            ArcaneAttack.HasValue ||
+            FireAttack.HasValue ||
+            BoltAttack.HasValue ||
+            BulletUse.HasValue ||
+            RapidPoison.HasValue ||
+            ImprintsNormal.HasValue ||
+            ImprintsUncanny.HasValue ||
+            ImprintsLost.HasValue ||
+            Rally.HasValue ||
+            StrengthRequirement.HasValue ||
+            SkillRequirement.HasValue ||
+            BloodtingeRequirement.HasValue ||
+            ArcaneRequirement.HasValue ||
+            MaxUpgradeAttack.HasValue;
+    }
+}
