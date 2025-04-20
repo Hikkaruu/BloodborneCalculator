b2f87c09f97243e4f7ce8bfff85d15f7a75aa81f
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-04-20 19:46:46 +0200

    Revert "Add Id to Update DTOs"
    This reverts commit b50b0043628e7318ed398dc8f96cf2ca096e7852.

--- backend/api/Models/DTOs/TricksterWeapon/UpdateTricksterWeaponDto.cs
+++ backend/api/Models/DTOs/TricksterWeapon/UpdateTricksterWeaponDto.cs
@@ -5,9 +5,6 @@ namespace api.Models.DTOs.TricksterWeapon
 {
     public class UpdateTricksterWeaponDto
     {
-        [Required]
-        public int Id { get; set; }
-
         [MaxLength(50, ErrorMessage = "Name can't be longer than 50 characters")]
         public string? Name { get; set; }
 
