0a446a456f3c0a72e5ecdeb03353db07160bbcc0
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-04-23 20:40:29 +0200

    Implement AttackController

---
+++ backend/api/Controllers/AttackController.cs
@@ -0,0 +1,108 @@
+﻿using api.Interfaces;
+using api.Models.DTOs.Attack;
+using api.Models.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AttackController : ControllerBase
+    {
+        private readonly IAttackService _attackService;
+        public AttackController(IAttackService attackService)
+        {
+            _attackService = attackService ?? throw new ArgumentNullException(nameof(attackService));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            try
+            {
+                var attack = await _attackService.GetAttackByIdAsync(id);
+
+                return Ok(attack);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                var attacks = await _attackService.GetAllAttacksAsync();
+
+                return Ok(attacks);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] CreateAttackDto createDto)
+        {
+            try
+            {
+                var attack = await _attackService.CreateAttackAsync(createDto);
+
+                return CreatedAtAction(nameof(GetByIdAsync), new { id = attack.Id }, attack);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(int id, [FromBody] UpdateAttackDto updateDto)
+        {
+            try
+            {
+                var updatedAttack = await _attackService.UpdateAttackAsync(id, updateDto);
+
+                return Ok(updatedAttack);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            try
+            {
+                await _attackService.DeleteAttackAsync(id);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("filter")]
+        public async Task<IActionResult> GetByFilterAsync([FromQuery] AttackFilter filter)
+        {
+            try
+            {
+                var attack = await _attackService.GetAttacksByFilterAsync(filter);
+
+                return Ok(attack);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+    }
+}
