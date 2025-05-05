a14d1bf96f1db538d337a0672ef87b2f19d97e8f
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-05-05 18:02:24 +0200

    Add main files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { OptimalClassComponent } from './pages/optimal-class/optimal-class.component';
+import { BossComponent } from './pages/boss/boss.component';
+import { FirearmsComponent } from './pages/firearms/firearms.component';
+import { TricksterWeaponsComponent } from './pages/trickster-weapons/trickster-weapons.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'optimal-class',
+    component: OptimalClassComponent,
+  },
+  {
+    path: 'bosses',
+    component: BossComponent,
+  },
+  {
+    path: 'firearms',
+    component: FirearmsComponent,
+  },
+  {
+    path: 'trickster-weapons',
+    component: TricksterWeaponsComponent,
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' },
+];
