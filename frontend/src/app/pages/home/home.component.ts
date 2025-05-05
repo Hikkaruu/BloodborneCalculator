c144ad202eae8478c1c5d5c7bb90d0bcc0110faf
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-05-05 18:01:18 +0200

    Add home-component

---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  navigate(path: string) {
+    this.router.navigate([path]);
+  }
+}
