90f709c5d645194c0b4ba4b49ca1f359699c6928
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-05-06 18:16:34 +0200

    Add Backend host to CORS

--- backend/api/Configuration/DependencyInjection.cs
+++ backend/api/Configuration/DependencyInjection.cs
@@ -68,13 +68,16 @@ namespace api.Configuration
 
         public static IServiceCollection AddCorsOptions(this IServiceCollection services)
         {
+            var productionCorsHost = Environment.GetEnvironmentVariable("PRODUCTION_CORS_HOST")!;
 
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowAngular",
                     policy =>
                     {
-                        policy.WithOrigins("http://localhost:4200")
+                        policy.WithOrigins(
+                                "http://localhost:4200",
+                                productionCorsHost)
                               .AllowAnyHeader()
                               .AllowAnyMethod();
                     });
